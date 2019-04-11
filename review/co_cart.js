4555717f9cf9427d621dbd4f7de0a068cf46b26e
BethanyDuMontelle <bdumontelle@stcharlessd.org>
2019-04-11 08:40:15 -0500

    modified:   review/co_cart.js 	deleted:    review/co_cart_txt.html 	deleted:    review/co_cart_txt.js 	deleted:    review/co_credit_txt.html 	deleted:    review/co_credit_txt.js

--- review/co_cart.js
+++ review/co_cart.js
@@ -7,8 +7,8 @@
 
    Shopping Cart Form Script
    
-   Author: 
-   Date:   
+   Author: Aisha Jahlan
+   Date:   2018-03-01
    
    Filename: co_cart.js
    
@@ -28,24 +28,28 @@
    
 */ 
 
+
 window.addEventListener("load", function() {
-	
-	//initial cost of the order
-	calcCart();
-	
-	//form event handlers
-	cartForm.elements.modelQty.onchange = calcCart;
-	
-	var shippingOptions = document.querySelectorAll('input[name="shipping"]');
-	for (var i = 0; i < shippingOptions.length; i++) {
-		shippingOptions[i].onclick = calcCart;
-	}
+   var cartForm = document.forms.cart;
+   
+   // Calculate the cost of the order
+   calcCart();
+   
+   // Event handlers for the web form
+   cartForm.elements.modelQty.onchange = calcCart;
+
+   var shippingOptions = document.querySelectorAll('input[name="shipping"]');
+   for (var i = 0; i < shippingOptions.length; i++) {
+      shippingOptions[i].onclick = calcCart;
+   }
+  
 });
-	
+
+
 function calcCart() {
    var cartForm = document.forms.cart;
    
-   // inital cost of the order
+   // Calculate the inital cost of the order
    var mCost = cartForm.elements.modelCost.value;
    var qIndex = cartForm.elements.modelQty.selectedIndex;
    var quantity = cartForm.elements.modelQty[qIndex].value;
@@ -54,26 +58,25 @@ function calcCart() {
    var orderCost = mCost*quantity;
    cartForm.elements.orderCost.value = formatUSCurrency(orderCost);
 
-   // cost of shipping
+   // Retrieve the cost of shipping
    var shipCost = document.querySelector('input[name="shipping"]:checked').value*quantity;
    cartForm.elements.shippingCost.value = formatNumber(shipCost, 2);
    
-   // order subtotal
+   // Calculate the order subtotal
    cartForm.elements.subTotal.value = formatNumber(orderCost + shipCost, 2);
    
-   // sales tax
+   // Calculate the sales tax
    var salesTax = 0.05*(orderCost + shipCost);
    cartForm.elements.salesTax.value = formatNumber(salesTax, 2);
    
-   // total order
+   // Calculate the cost of the total order
    var cartTotal = orderCost + shipCost + salesTax;
    cartForm.elements.cartTotal.value = formatUSCurrency(cartTotal);
    
-   // store the order details
+   // Store the order details
    cartForm.elements.shippingType.value = 
       document.querySelector('input[name="shipping"]:checked').nextSibling.nodeValue;
 }
-
 
 function formatNumber(val, decimals) {
    return val.toLocaleString(undefined, {minimumFractionDigits: decimals, 
